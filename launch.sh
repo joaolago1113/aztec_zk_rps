6a04edf6f6a84a93fbbf37db19e06563368c8ade
joaolago1113 <22820692+joaolago1113@users.noreply.github.com>
2025-01-25 13:28:41 +0000

    fix contract

--- launch.sh
+++ launch.sh
@@ -1,5 +1,7 @@
 cd src/contracts/
 aztec-nargo compile
+echo "done compile"
 aztec codegen -o src/artifacts target
+echo "done codegen"
 cd ../../
 ./deployment_command.sh | python3 generate_config.py
